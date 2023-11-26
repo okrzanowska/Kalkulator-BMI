edde53bc20f83bb67e501628e023d7d77a10b942
oliwi <oliwi@OLIWIA>
2023-11-26 10:00:21 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace KalkulatorBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oblicz swój wskaźnik masy ciała.");
+            Console.WriteLine("Podążaj za instrukcjami kalkulatora.");
+
+            Console.WriteLine("Podaj swoją masę ciała [kg]:");
+            double wagaKg = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Podaj swój wzrost [cm]:");
+            double wzrostCm = Convert.ToDouble(Console.ReadLine());
+
+            double wzrostM = wzrostCm / 100;
+
+            double bmi = wagaKg / Math.Pow(wzrostM, 2);
+
+            Console.WriteLine($"Twoje BMI wynosi {bmi}.");
+        }
+    }
+}
+
